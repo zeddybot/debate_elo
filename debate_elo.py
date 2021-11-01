bdc68371a9d442f456f1aff7942690984a1a4120
Sameer Nayyar <sameernayyar2003@gmail.com>
2021-11-01 11:28:29 -0700

    Updated CLI documentation

--- debate_elo.py
+++ debate_elo.py
@@ -11,11 +11,7 @@ def main():
     """
     args = parse_args()
     with args.infile as infile, args.outfile as outfile:
-        args.action(
-            infile,
-            outfile,
-            append=args.append
-        )
+        args.action(infile, outfile, append=args.append)
     print("Done.")
 
 
@@ -23,35 +19,81 @@ def parse_args():
     """
     Generate a return a parse object for this program.
     """
-    parser = argparse.ArgumentParser(
-        description='Download debate round data and calculating ELO rankings.')
+    parser = argparse.ArgumentParser(description=(
+        'The purpose of this program is to allow for the downloading of '
+        'debate data and the performing of simple analysis on that data. '
+        'Currently, the functionalities exist to rank all debaters present '
+        'in the dataset with the ELO ranking system and to output the '
+        'rankings in a human-friendly format.')
+    )
     action_group = parser.add_mutually_exclusive_group(required=True)
     action_group.add_argument(
         '-d', '--download',
         dest='action',
         action='store_const',
         const=save_round_data,
+        help=(
+            'Flag to download all the round data from the tournaments JSON '
+            'in the infile and save/update the result JSON in the outfile.'
+        ),
     )
     action_group.add_argument(
         '-c', '--calculate',
         dest='action',
         action='store_const',
         const=save_elos,
+        help=(
+            'Flag to calculate the ELOs of all debaters present in the rounds '
+            'JSON in the infile and save/update the resulting JSON of ELOs in '
+            'the outfile.'
+        ),
     )
     action_group.add_argument(
         '-r', '--rank',
         dest='action',
         action='store_const',
         const=save_rankings,
+        help=(
+            'Flag to convert the JSON of ELOs in the infile into a human-readable '
+            'format and save the resulting text in the outfile.'
+        ),
+
     )
     parser.add_argument(
         '-a',
         '--append',
         action='store_true',
-        help='Avoids overwriting data.',
+        help=(
+            'Switches to append mode. When this option is disabled (default), '
+            'running the program will immediately truncate and overwrite the '
+            'outfile. When --append is enabled alongside --download, the program '
+            'will first store the JSON in the outfile and will avoid re-downloading '
+            'data from tournaments already present in the outfile JSON. After all '
+            'relevant data is downloaded, the outfile JSON is updated to contain '
+            'data from all tournaments in the infile JSON. When --append is '
+            'enabled alongside --calculate, the program will use the ELO JSON in '
+            'the outfile as the set of starting ELOs for the ELO calculation. '
+            'When --append is enabaled alongside --rank, no behavior is changed.'
+        ),
     )
-    parser.add_argument('infile', type=argparse.FileType('r'))
-    parser.add_argument('outfile', type=argparse.FileType('r+'))
+    parser.add_argument(
+        'infile',
+        type=argparse.FileType('r'),
+        help=(
+            'Input file. This will always contain a JSON whose shape varies '
+            'depending on the chosen action.'
+        ),
+    )
+    parser.add_argument(
+        'outfile',
+        type=argparse.FileType('r+'),
+        help=(
+            'Output file. When append mode is enabled and either the download or '
+            'calculate actions are chosen, this file may contain a JSON of '
+            'relevant data. Otherwise, the file contents are not relevant. The file '
+            'must exist before the execution of the program.'
+        ),
+    )
 
     return parser.parse_args()
 
